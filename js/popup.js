be190496890ae5d08c9400ade4938f2613ff2658
s-c-23 <shyamli.channa@utexas.edu>
2023-04-23 14:05:18 -0500

    Updating attributes

--- js/popup.js
+++ js/popup.js
@@ -5,7 +5,12 @@ async function logJSONData() {
   console.log(jsonData[0]['thumbnail']);
     for(var i  = 1; i <= 6; i++) {
       var image_id = 'image_op_' + i + "";
+      var brand_id = 'brand_' + i + "";
+      var price_id = 'price_' + i + "";
       document.getElementById(image_id).src=(""+jsonData[i-1]['thumbnail']+"");
+      document.getElementById(image_id).href=(""+jsonData[i-1]['link']+"");
+      document.getElementById(brand_id).innerHTML=(""+jsonData[i-1]['source']+"");
+      document.getElementById(price_id).innerHTML=(""+jsonData[i-1]['price']+"");
     }
 }
 
