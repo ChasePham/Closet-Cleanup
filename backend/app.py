0c30132525fd7609c0900ec655fc349371227db1
s-c-23 <shyamli.channa@utexas.edu>
2023-04-23 13:56:37 -0500

    Shopping works

--- backend/app.py
+++ backend/app.py
@@ -2,63 +2,27 @@ from flask import Flask
 import pandas as pd
 import numpy as np
 from serpapi import GoogleSearch
+import requests, lxml, os, json
 import os, json
 app = Flask(__name__)
 
-@app.route('/data')
+@app.route('/')
 def get_data():
     params = {
-     "api_key": "de13e5e6e842d938c52ef7697f1d26055cd17112dbb83081e9e4d619f34eca5e",
-     "engine": "google",
      "q": "brown shoes",
+     "tbm" : "shop",
      "location": "Austin, Texas, United States",
-     "google_domain": "google.com",
-     "gl": "us",
      "hl": "en",
-     "ijn": 0,
-    "tbm": "isch"
+     "gl": "us",
+     "api_key": "de13e5e6e842d938c52ef7697f1d26055cd17112dbb83081e9e4d619f34eca5e",
     }
 
     search = GoogleSearch(params)
+    results = search.get_dict()
 
-    # Get Pictures and website links dataframe"""
-
-    # Convert to df
-    # Saves the picture links
-    image_results = []
-    # Saves the source links
-    image_source_links = []
-
-
-    while True:
-        results = search.get_dict()
-        if "error" not in results:
-
-        # Goes through 100 at a time I believe
-            for i in range(len(results['images_results'])):
-
-                current = results['images_results'][i]
-                curr_original_pic = current['original']
-                curr_link = current['link']
-                if curr_original_pic not in image_results:
-                    image_results.append(curr_original_pic)
-                    image_source_links.append(curr_link)
-
-            # Limit to how many similar clothing items we want
-            if len(image_results) == 10:
-             break
-
-            params['ijn'] += 1
-        else:
-            break
-    # Same as above
-        if len(image_results) == 10:
-            break
-
-
-    # print(image_results)
-    # print(image_source_links)
-    # print(len(image_results))
+    google_shopping_data = results["shopping_results"]
+    
+    print(json.dumps(google_shopping_data, indent=2, ensure_ascii=False)) 
 
     ethical_brands = ['larelaxed','wearwell','veneka','freespiritbrand',
     'etica','knickey','eclipse','harvestandmill','thestandardstich',
@@ -71,24 +35,14 @@ def get_data():
     'orbasics','swedishstockings','armedangels','intoadesign',
     'nudiajeans','lilianvontrapp','silentwaveindigo','appleoakfibreworks',
     'jackfruit','etiko','flarestreet','citizenwolf','joyya','commongood','rupahus','dorsu','nonasties']
-    
-    image_results_final = []
-    image_source_links_final = []
 
     #FIXME Test this to fix if it works. To test, input the top two list above this one into the data frame return statement
-    for i in range(len(image_source_links)):
-        current = image_source_links[i].lower()
-        for j in range(len(ethical_brands)):
-            if ethical_brands[j] in current:
-                image_results_final.append(image_results[i])
-                image_source_links_final.append(image_source_links[i])
-                break
+    # print(json.dumps(google_shopping_data, indent=2, ensure_ascii=False)) 
         
-    return pd.DataFrame({'img_pic': image_results_final, 'img_source_link': image_source_links_final}).to_json()
+    return (json.dumps(google_shopping_data, indent=2, ensure_ascii=False))
 
     # Will most likely need a large list containing the most sustainable clothing brands OR a list containing non-sustainable clothes. (2 ways to do this)
 
-    """Extracting Sustainable Clothing"""
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000)
+    # get_data()
--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,19 @@
+async function logJSONData() {
+  const response = await fetch("http://localhost:4000");
+  const jsonData = await response.json();
+  console.log(jsonData);
+  console.log(jsonData[0]['thumbnail']);
+    for(var i  = 1; i <= 6; i++) {
+      var image_id = 'image_op_' + i + "";
+      document.getElementById(image_id).src=(""+jsonData[i-1]['thumbnail']+"");
+    }
+}
 
-  logJSONData()
-  async function logJSONData() {
-    const response = await fetch("http://localhost:4000");
-    const jsonData = await response.json();
-    console.log(jsonData);
-  }
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/data');
-  xhr.onload = function() {
-      if (xhr.status === 200) {
-          var data = JSON.parse(xhr.responseText);
-          console.log(data.message);
-      }
-  };
-  xhr.send();
-  function sayHello() {
-    console.log("Hello, World!");
-  }
+logJSONData()
+
+// // function to get the tabId of the current tab
+// async function getTabId() {
+//   let queryOptions = { active: true, currentWindow: true };
+//   let tabs = await chrome.tabs.query(queryOptions);
+//   return tabs[0].id;
+// }
