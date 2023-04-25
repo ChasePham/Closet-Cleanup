2fdaf58bd92c05e0eeab06aab322d219fa44342c
ChasePham <chase9659@gmail.com>
2023-04-24 22:22:08 -0500

    get suggestions

--- backend/app.py
+++ backend/app.py
@@ -9,20 +9,13 @@ app = Flask(__name__)
 @app.route('/')
 def get_data():
     params = {
-     "q": "brown shoes",
+     "q": "Striped cropped shirt",
      "tbm" : "shop",
      "location": "Austin, Texas, United States",
      "hl": "en",
      "gl": "us",
      "api_key": "de13e5e6e842d938c52ef7697f1d26055cd17112dbb83081e9e4d619f34eca5e",
     }
-
-    search = GoogleSearch(params)
-    results = search.get_dict()
-
-    google_shopping_data = results["shopping_results"]
-    
-    print(json.dumps(google_shopping_data, indent=2, ensure_ascii=False)) 
 
     ethical_brands = ['larelaxed','wearwell','veneka','freespiritbrand',
     'etica','knickey','eclipse','harvestandmill','thestandardstich',
@@ -55,10 +48,28 @@ def get_data():
     'duchxfashion','seeker','panamunaproject','annehermine',
     'nightswim','thegoodtee','fairindigo']
 
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+    google_shopping_data = results["shopping_results"]
+
+    thumbnails = []
+    product_links = []
+
+    for i in google_shopping_data:
+        current_thumbnail = i['thumbnail']
+        current_link = i['link']
+        source = i['source']
+        if source.lower() in ethical_brands:
+            thumbnails.append(current_thumbnail)
+            product_links.append(current_link)
+
+    print(json.dumps(product_links, indent=2, ensure_ascii=False)) 
     #FIXME Test this to fix if it works. To test, input the top two list above this one into the data frame return statement
     # print(json.dumps(google_shopping_data, indent=2, ensure_ascii=False)) 
         
-    return (json.dumps(google_shopping_data, indent=2, ensure_ascii=False))
+    return (json.dumps(product_links, indent=2, ensure_ascii=False))
 
     # Will most likely need a large list containing the most sustainable clothing brands OR a list containing non-sustainable clothes. (2 ways to do this)
 
